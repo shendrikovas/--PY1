07437a5dcb55b8b05ea3f069c91156e8a1d31a5e
shendrikovas <115953354+shendrikovas@users.noreply.github.com>
2022-11-28 16:56:25 +0300

    лабораторная 5

--- task3.py
+++ task3.py
@@ -1,17 +1,15 @@
-# TODO написать функцию для получения списка уникальных целых чисел
-from random import randint
-
+# TODO написать функцию для получения списка уникальных целых чиселimport random
+import random
 
 def get_unique_list_numbers() -> list[int]:
-    size = 15
-    min_ = -10
-    max_ = 10
-    list_unique_num = []
-    while len(list_unique_num) != size:
-        rand_val = randint(min_, max_)
-        if rand_val not in list_unique_num:
-            list_unique_num.append(rand_val)
-    return list_unique_num
+    num = 15
+    list_ = [random.randint(-10, 10) for x in range(num)]
+    list_unique = set(list_)
+    while len(list_unique) < num:
+        list_ = list_ + [random.randint(-10, 10) for x in range(num - len(list_unique))]
+        list_unique = set(list_)
+    list_ = [numb for numb in list_unique]
+    return list_
 
 
 list_unique_numbers = get_unique_list_numbers()
