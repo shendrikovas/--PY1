a1ae572e4b15ec32aa59b3b71cb5f2323ca7f5b1
shendrikovas <115953354+shendrikovas@users.noreply.github.com>
2022-11-28 16:40:45 +0300

    лабораторная работа 5

--- task3.py
+++ task3.py
@@ -1,5 +1,4 @@
-def get_unique_list_numbers() -> list[int]:
-    ...  # TODO написать функцию для получения списка уникальных целых чисел
+# TODO написать функцию для получения списка уникальных целых чисел
 from random import randint
 
 
